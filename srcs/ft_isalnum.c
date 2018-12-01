913984830313b1ce68ac005fed9095f6c202f883
Manfred dondarrion Balon swann <mbalon-s@e1r5p6.21-school.ru>
2018-12-01 19:04:41 +0300

    finished isalnum

--- srcs/ft_isalnum.c
+++ srcs/ft_isalnum.c
@@ -1,24 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbalon-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/01 18:14:51 by mbalon-s          #+#    #+#             */
-/*   Updated: 2018/12/01 19:04:30 by mbalon-s         ###   ########.fr       */
+/*   Created: 2018/12/01 19:03:55 by mbalon-s          #+#    #+#             */
+/*   Updated: 2018/12/01 19:04:28 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <string.h>
+#include "libft.h"
 
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t len);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
-int		ft_isalpha(int c);
-int		ft_isgidit(int c);
-int		ft_isalnum(int c);
-#endif
+int		ft_isalnum(int c)
+{
+	return (ft_isalpha(c) || ft_isdigit(c));
+}
