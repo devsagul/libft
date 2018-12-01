9d8ca9c030cce5fac3abea0147ad8ba87c8597e8
Manfred dondarrion Balon swann <mbalon-s@e1r5p6.21-school.ru>
2018-12-01 18:28:07 +0300

    finished memset

---
+++ srcs/ft_memset.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbalon-s <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/01 18:15:03 by mbalon-s          #+#    #+#             */
+/*   Updated: 2018/12/01 18:27:37 by mbalon-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char *tmp;
+
+	tmp = (unsigned char *)b;
+	while (len > 0)
+	{
+		*tmp = c;
+		tmp++;
+		len--;
+	}
+	return (b);
+}
