620125e531f8a2d5501a80330713a1cc2c4acfbf
Manfred dondarrion Balon swann <mbalon-s@e3r4p7.21-school.ru>
2018-12-04 22:27:42 +0300

    finished ft_strnequ

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mbalon-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/01 18:14:51 by mbalon-s          #+#    #+#             */
-/*   Updated: 2018/12/04 22:26:16 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2018/12/04 22:27:32 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,4 +51,5 @@ void	ft_striteri(char *s, void (*f) (unsigned int, char *));
 char	*ft_strmap(char const *s, char (*f) (char *));
 char	*ft_strmapi(char const *s, char (*f) (unsigned int, char *));
 int		ft_strequ(char const *s1, char const *s2);
+int		ft_strnequ(char const *s1, char const *s2, size_t n);
 #endif
