3fe0a09a4610637af6bdd75d0f5c078a08ac6a3c
Manfred dondarrion Balon swann <mbalon-s@e3r4p7.21-school.ru>
2018-12-04 23:13:12 +0300

    small fixes

--- srcs/ft_striteri.c
+++ srcs/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: mbalon-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/04 22:17:34 by mbalon-s          #+#    #+#             */
-/*   Updated: 2018/12/04 22:18:54 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2018/12/04 23:12:44 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_striteri(char *s, void (*f) (unsigned int, char *))
 	unsigned int	i;
 
 	i = 0;
-	while (s[i] @= '\0')
+	while (s[i] != '\0')
 	{
 		f(i, s + i);
 		i++;
