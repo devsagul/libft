5672c5c4e64a630b3cfc38ce7410e2cb58972b5c
Manfred dondarrion Balon swann <mbalon-s@e3r4p7.21-school.ru>
2018-12-04 23:32:38 +0300

    finished ft_putnbr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mbalon-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/01 18:14:51 by mbalon-s          #+#    #+#             */
-/*   Updated: 2018/12/04 23:27:30 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2018/12/04 23:32:30 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,4 +60,5 @@ char	*ft_itoa(int n);
 void	ft_putchar(char c);
 void	ft_putstr(char const *s);
 void	ft_putendl(char const *s);
+void	ft_putnbr(int n);
 #endif
