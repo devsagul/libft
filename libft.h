da4cd64bb72a54b832b82b9dfbb57c381cc55862
Manfred dondarrion Balon swann <mbalon-s@e3r4p7.21-school.ru>
2018-12-04 23:19:40 +0300

    finsihed ft_itoa

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mbalon-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/01 18:14:51 by mbalon-s          #+#    #+#             */
-/*   Updated: 2018/12/04 23:12:00 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2018/12/04 23:19:30 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,4 +56,5 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s);
 char	**ft_strsplit(char const *s, char c);
+char	*ft_itoa(int n);
 #endif
---
+++ srcs/ft_itoa.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbalon-s <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/04 23:14:44 by mbalon-s          #+#    #+#             */
+/*   Updated: 2018/12/04 23:19:30 by mbalon-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "libft.h"
+
+static unsigned int	count_digits(int n);
+
+char				*ft_itoa(int n)
+{
+	unsigned int	count;
+	char			*res;
+
+	count = count digits(n);
+	res = (char *)malloc(sizeof(char) * (count + 1));
+	if (res == NULL)
+		return (NULL);
+	res[count] = '\0';
+	if (n == 0)
+	{
+		res[0] = '0';
+		return (res);
+	}
+	if (n < 0)
+		res[0] = '-';
+	count--;
+	while (n != 0)
+	{
+		res[count] = '0' + n % 10;
+		n /= 10;
+		count--;
+	}
+	return (res);
+}
+
+static unsigned int	count_digits(int n)
+{
+	unsigned int	res;
+
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		res = 1;
+	else
+		res = 0;
+	while (n != 0)
+	{
+		res++;
+		n /= 10;
+	}
+	return (res);
+}
