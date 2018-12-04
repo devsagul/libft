2ecda6e4629da33bda4545b6b61022e2efcebdf8
Manfred dondarrion Balon swann <mbalon-s@e3r4p7.21-school.ru>
2018-12-04 22:24:00 +0300

    finished ft_strmapi

--- srcs/ft_strmapi.c
+++ srcs/ft_strmapi.c
@@ -1,22 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbalon-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/04 22:19:52 by mbalon-s          #+#    #+#             */
+/*   Created: 2018/12/04 22:22:59 by mbalon-s          #+#    #+#             */
 /*   Updated: 2018/12/04 22:23:50 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strmap(char const *s, char (*f) (char *))
+char	*ft_strmapi(char const *s, char (*f) (unsigned int, char *))
 {
 	char	*res;
 
 	res = ft_strdup(s);
-	ft_striter(res, f);
+	ft_striteri(res, f);
 	return (res);
 }
