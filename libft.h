b23928a1f59137710a2abf53216995c6ff65d3e0
Manfred dondarrion Balon swann <mbalon-s@e3r6p7.21-school.ru>
2018-12-06 19:05:02 +0300

    finished ft_lstnew

--- libft.h
+++ libft.h
@@ -6,13 +6,20 @@
 /*   By: mbalon-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/01 18:14:51 by mbalon-s          #+#    #+#             */
-/*   Updated: 2018/12/04 23:59:30 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2018/12/06 19:04:50 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <string.h>
+
+typedef struct		s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
 
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t len);
@@ -65,4 +72,5 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+t_list	*ft_lstnew(void const *content, size_t content_size);
 #endif
---
+++ srcs/ft_lstnew.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbalon-s <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/06 19:01:18 by mbalon-s          #+#    #+#             */
+/*   Updated: 2018/12/06 19:04:49 by mbalon-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "libft.h"
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*res;
+
+	res = (t_list *)malloc(sizeof(t_list));
+	if (res == NULL)
+		return (NULL);
+	if (content == NULL)
+	{
+		res->content = NULL;
+		res->content_size = 0;
+	}
+	else
+	{
+		res->content = ft_memcpy(res->content, content, content_size);
+		if (res->content == NULL)
+		{
+			free(res);
+			return NULL;
+		}
+		res->content_size = content_size;
+	}
+	return (res);
+}
