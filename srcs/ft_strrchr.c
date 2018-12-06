6d09a6de0abeae7510de0a79136003b45a51887b
Manfred dondarrion Balon swann <mbalon-s@e3r6p7.21-school.ru>
2018-12-06 21:55:31 +0300

    fixed timeouts strchr

--- srcs/ft_strrchr.c
+++ srcs/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: mbalon-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 19:45:07 by mbalon-s          #+#    #+#             */
-/*   Updated: 2018/12/04 21:27:08 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2018/12/06 21:55:05 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,16 @@ char	*ft_strrchr(const char *s, int c)
 	char	*res;
 	char	*tmp;
 
-	res = strchr(s, c);
+	res = ft_strchr(s, c);
 	if (res == NULL)
 		return (NULL);
-	tmp = strchr(res, c);
+	if (c == '\0')
+		return (res);
+	tmp = ft_strchr(res + 1, c);
 	while (tmp != NULL)
 	{
 		res = tmp;
-		tmp = strchr(res, c);
+		tmp = ft_strchr(res + 1, c);
 	}
 	return (res);
 }
