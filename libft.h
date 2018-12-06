931b28f2ea2049ef86bfd148801471437b529cf5
Manfred dondarrion Balon swann <mbalon-s@e3r6p7.21-school.ru>
2018-12-06 21:43:30 +0300

    fixes

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mbalon-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/01 18:14:51 by mbalon-s          #+#    #+#             */
-/*   Updated: 2018/12/06 19:51:06 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2018/12/06 21:42:33 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,7 @@ void	ft_strdel(char **as);
 void	ft_strclr(char *s);
 void	ft_striter(char *s, void (*f) (char *));
 void	ft_striteri(char *s, void (*f) (unsigned int, char *));
-char	*ft_strmap(char const *s, char (*f) (char *));
+char	*ft_strmap(char const *s, char (*f) (char));
 char	*ft_strmapi(char const *s, char (*f) (unsigned int, char *));
 int		ft_strequ(char const *s1, char const *s2);
 int		ft_strnequ(char const *s1, char const *s2, size_t n);
--- srcs/ft_strmap.c
+++ srcs/ft_strmap.c
@@ -6,14 +6,14 @@
 /*   By: mbalon-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/04 22:19:52 by mbalon-s          #+#    #+#             */
-/*   Updated: 2018/12/06 21:13:50 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2018/12/06 21:43:24 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "libft.h"
 
-char	*ft_strmap(char const *s, char (*f) (char *))
+char	*ft_strmap(char const *s, char (*f) (char))
 {
 	char	*res;
 	size_t	i;
@@ -21,10 +21,12 @@ char	*ft_strmap(char const *s, char (*f) (char *))
 	if (s == NULL || f == NULL)
 		return (NULL);
 	res = ft_strdup(s);
+	if (res == NULL)
+		return (NULL);
 	i = 0;
 	while (res[i] != '\0')
 	{
-		f(res + i);
+		res[i] = f(res[i]);
 		i++;
 	}
 	return (res);
