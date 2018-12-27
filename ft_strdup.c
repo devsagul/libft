80191c1ce3de3fde4c245b790d928db315939749
Manfred dondarrion Balon swann <mbalon-s@e3r5p7.21-school.ru>
2018-12-27 20:56:37 +0300

    fixed ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: mbalon-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 18:12:27 by mbalon-s          #+#    #+#             */
-/*   Updated: 2018/12/27 20:31:55 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2018/12/27 20:56:23 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,11 @@
 char	*ft_strdup(const char *s1)
 {
 	size_t	len;
-	size_t	i;
 	char	*res;
 
 	len = ft_strlen(s1);
 	res = (char *)malloc(sizeof(char) * (len + 1));
 	if (res == NULL)
 		return (NULL);
-	res[len] = '\0';
-	i = 0;
-	while (i < len)
-	{
-		res[i] = s1[i];
-		i++;
-	}
-	return (res);
+	return (ft_strcpy(res, s1));
 }
