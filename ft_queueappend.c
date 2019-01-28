f219a9c538e501be69228938327097e07a112ccf
Manfred dondarrion Balon swann <mbalon-s@e2r4p3.21-school.ru>
2019-01-28 22:27:19 +0300

    fixed ft_queuecreate

--- ft_queueappend.c
+++ ft_queueappend.c
@@ -6,7 +6,7 @@
 /*   By: mbalon-s <mbalon-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 21:55:50 by mbalon-s          #+#    #+#             */
-/*   Updated: 2019/01/28 22:26:29 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2019/01/28 22:27:06 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void				ft_queueappend(t_queue *queue, t_list *item)
 {
-    if (queue = NULL || item == NULL)
+    if (queue == NULL || item == NULL)
         return ;
     if (queue->size == 0)
         queue->head = item;
