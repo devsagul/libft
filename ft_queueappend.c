81db873d5d1874b1e519e09439695e5a71ceec0c
Manfred dondarrion Balon swann <mbalon-s@e2r4p3.21-school.ru>
2019-01-28 23:00:14 +0300

    fixes

--- ft_queueappend.c
+++ ft_queueappend.c
@@ -6,12 +6,12 @@
 /*   By: mbalon-s <mbalon-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 21:55:50 by mbalon-s          #+#    #+#             */
-/*   Updated: 2019/01/28 22:59:41 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2019/01/28 23:00:06 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
+#include <stdio.h>
 
 void				ft_queueappend(t_queue *queue, t_list *item)
 {
