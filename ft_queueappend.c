1ac8b28793edd641686c1200a58e9dd21b93883d
Manfred dondarrion Balon swann <mbalon-s@e2r4p3.21-school.ru>
2019-01-28 23:02:32 +0300

    debug

--- ft_queueappend.c
+++ ft_queueappend.c
@@ -6,7 +6,7 @@
 /*   By: mbalon-s <mbalon-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 21:55:50 by mbalon-s          #+#    #+#             */
-/*   Updated: 2019/01/28 23:01:40 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2019/01/28 23:02:24 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void				ft_queueappend(t_queue *queue, t_list *item)
         queue->tail = item;
     }
     queue->size += ft_lstlength(item);
-    printf("%uz", item->next);
+    printf("%p", item->next);
     queue->tail->next = item;
     while (queue->tail->next != NULL)
     {
