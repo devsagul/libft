aec6538d6e47c8c942a2629f62975e89166b9da6
Manfred dondarrion Balon swann <mbalon-s@e2r4p3.21-school.ru>
2019-01-28 21:52:44 +0300

    fixed ft_queuecreate

--- ft_queuecreate.c
+++ ft_queuecreate.c
@@ -6,7 +6,7 @@
 /*   By: mbalon-s <mbalon-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 21:41:59 by mbalon-s          #+#    #+#             */
-/*   Updated: 2019/01/28 21:47:16 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2019/01/28 21:51:52 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_queue				ft_queuecreate(void)
 {
     t_queue res;
 
-    res->size = 0;
-    res->head = NULL;
+    res.size = 0;
+    res.head = NULL;
     return (res);
 }
