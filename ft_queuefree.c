d5d764771c90433285a3970480c6f6e9b5d5dda7
Manfred dondarrion Balon swann <mbalon-s@e2r4p3.21-school.ru>
2019-01-28 23:32:55 +0300

    fixed ft_queuefree

--- ft_queuefree.c
+++ ft_queuefree.c
@@ -6,7 +6,7 @@
 /*   By: mbalon-s <mbalon-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 23:27:13 by mbalon-s          #+#    #+#             */
-/*   Updated: 2019/01/28 23:27:31 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2019/01/28 23:32:30 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 void				ft_queuefree(t_queue *queue, void (*del) (void *, size_t))
 {
+    t_list  *tmp;
 
+    if (queue == NULL || del == NULL)
+        return ;
+    while (queue.size != 0)
+    {
+        tmp = ft_queuepop(&queue);
+        del(tmp->content, tmp->size);
+        free(tmp);
+    }
 }
