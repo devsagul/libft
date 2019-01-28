799c419a7becf0702a47c6b6cb73ea97d71c40bb
Manfred dondarrion Balon swann <mbalon-s@e2r4p3.21-school.ru>
2019-01-28 22:43:35 +0300

    fixed ft_queuepop

--- ft_queuepop.c
+++ ft_queuepop.c
@@ -6,7 +6,7 @@
 /*   By: mbalon-s <mbalon-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 22:33:29 by mbalon-s          #+#    #+#             */
-/*   Updated: 2019/01/28 22:40:09 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2019/01/28 22:43:09 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 t_list				*ft_queuepop(t_queue *queue)
 {
+    t_list  *res;
+
+    if (queue == NULL)
+        return (NULL);
+    if (queue->size == 0)
+        return (NULL);
+    res = queue->head;
+    queue->head = res->next;
+    res->next = NULL;
+    queue->size--;
     return (queue->head);
 }
