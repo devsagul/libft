fbd169373f08077cfc9e874b9738bbd855bd1c4b
Manfred dondarrion Balon swann <mbalon-s@e2r4p4.21-school.ru>
2019-01-29 17:19:17 +0300

    add include into ft_stackfree.c

--- ft_stackfree.c
+++ ft_stackfree.c
@@ -6,10 +6,11 @@
 /*   By: mbalon-s <mbalon-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/29 17:06:47 by mbalon-s          #+#    #+#             */
-/*   Updated: 2019/01/29 17:17:18 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2019/01/29 17:19:02 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "libft.h"
 
 void				ft_stackfree(t_stack *stack, void (*del) (void *, size_t))
