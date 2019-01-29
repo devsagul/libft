6364dba449b3d1c0ea7fcdee1364fd7a42b36847
Manfred dondarrion Balon swann <mbalon-s@e2r4p4.21-school.ru>
2019-01-29 17:05:13 +0300

    fixed ft_stackpop.c

--- ft_stackpop.c
+++ ft_stackpop.c
@@ -6,7 +6,7 @@
 /*   By: mbalon-s <mbalon-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/29 17:00:31 by mbalon-s          #+#    #+#             */
-/*   Updated: 2019/01/29 17:02:13 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2019/01/29 17:04:59 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,13 @@
 
 t_list				*ft_stackpop(t_stack *stack)
 {
+    t_list  *res;
+
     if (stack->size == 0)
         return (NULL);
-    return ((t_list *)1);
+    stack->size--;
+    res = stack->head;
+    stack->head = stack->head->next;
+    res->next = NULL;
+    return (res);
 }
