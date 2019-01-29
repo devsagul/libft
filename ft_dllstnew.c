79d43476edb03c281f10225f22e2db1d93b2f9ba
Manfred dondarrion Balon swann <mbalon-s@e4r5p3.21-school.ru>
2019-01-29 17:54:09 +0300

    rewrote ft_dllist

--- ft_dllstnew.c
+++ ft_dllstnew.c
@@ -6,7 +6,7 @@
 /*   By: mbalon-s <mbalon-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/29 17:51:02 by mbalon-s          #+#    #+#             */
-/*   Updated: 2019/01/29 17:51:50 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2019/01/29 17:53:50 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,28 @@
 
 t_dllist			*ft_dllstnew(void const *content, size_t content_size)
 {
-    t_dllist    *res;
+	t_dllist	*res;
 
-    res = ft_lstnew(content, content_size);
+	res = (t_dllist *)malloc(sizeof(t_dllist));
+	if (res == NULL)
+		return (NULL);
+	if (content == NULL || content_size == 0)
+	{
+		res->content = NULL;
+		res->content_size = 0;
+	}
+	else
+	{
+		res->content = (void *)malloc(content_size);
+		if (res->content == NULL)
+		{
+			free(res);
+			return (NULL);
+		}
+		res->content = ft_memcpy(res->content, content, content_size);
+		res->content_size = content_size;
+	}
+	res->next = NULL;
     res->prev = NULL;
-    return (res);
+	return (res);
 }
