5db994052cd53eaf7cf2789dfefbfd5f8e20febc
Manfred dondarrion Balon swann <mbalon-s@e2r4p4.21-school.ru>
2019-01-29 17:19:49 +0300

    fixed ft_stackfree.c

--- ft_stackfree.c
+++ ft_stackfree.c
@@ -6,7 +6,7 @@
 /*   By: mbalon-s <mbalon-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/29 17:06:47 by mbalon-s          #+#    #+#             */
-/*   Updated: 2019/01/29 17:19:02 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2019/01/29 17:19:35 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void				ft_stackfree(t_stack *stack, void (*del) (void *, size_t))
         return ;
     while(stack->size != 0)
     {
-        tmp = ft_stackpop(&stack);
+        tmp = ft_stackpop(stack);
         del(tmp->content, tmp->content_size);
         free(tmp);
     }    
