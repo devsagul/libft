d93eb6e8c70255835d2d9ecdddbd76de1a25e75b
Manfred dondarrion Balon swann <mbalon-s@e4r5p1.21-school.ru>
2019-01-29 20:58:45 +0300

    fixed ft_imatrixidentity

--- ft_imatrixidentity.c
+++ ft_imatrixidentity.c
@@ -6,7 +6,7 @@
 /*   By: mbalon-s <mbalon-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/29 20:49:28 by mbalon-s          #+#    #+#             */
-/*   Updated: 2019/01/29 20:50:15 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2019/01/29 20:58:24 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,27 @@
 t_imatrix			ft_imatrixidentity(size_t n)
 {
 	t_imatrix	res;
+	size_t		size;
+	size_t		i;
 
+	size = n * n;
+	res.n = 0;
+	res.m = 0;
+	if (size == 0 || size / n != n)
+	{
+		res.values = NULL;
+		return (res);
+	}
+	res.values = ft_memalloc(size);
+	if (res.values == NULL)
+		return (res);
+	res.n = n;
+	res.m = n;
+	i = 0;
+	while (i < n)
+	{
+		res.values[i * n + i] = 1;
+		i++;
+	}
 	return (res);
 }
