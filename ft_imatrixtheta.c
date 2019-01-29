e8b4874cf1b063304cabd31af79b7a4737ce85b4
Manfred dondarrion Balon swann <mbalon-s@e4r5p1.21-school.ru>
2019-01-29 20:42:46 +0300

    fixed ft_imatrixtheta

--- ft_imatrixtheta.c
+++ ft_imatrixtheta.c
@@ -6,7 +6,7 @@
 /*   By: mbalon-s <mbalon-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/29 20:36:57 by mbalon-s          #+#    #+#             */
-/*   Updated: 2019/01/29 20:41:35 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2019/01/29 20:42:34 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 t_imatrix			ft_imatrixtheta(size_t n, size_t m)
 {
 	size_t		size;
-	t_fmatrix	res;
+	t_imatrix	res;
 
 	size = n * m;
 	if (size == 0 || size / n != m)
