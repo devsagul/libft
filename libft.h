6ada72074baf4fcff3ae7e820c356d927be66f9b
Manfred dondarrion Balon swann <mbalon-s@e4r5p3.21-school.ru>
2019-01-30 17:38:41 +0300

    added ft_imatrix add

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mbalon-s <mbalon-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/01 18:14:51 by mbalon-s          #+#    #+#             */
-/*   Updated: 2019/01/30 17:08:34 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2019/01/30 17:22:49 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -133,6 +133,7 @@ int					ft_imatrixget(t_imatrix matrix,
 int					ft_imatrixset(t_imatrix matrix,
 								  size_t i, size_t j, int value);
 void				ft_imatrixfree(t_imatrix *matrix);
+t_imatrix			ft_imatrixadd(t_imatrix a, t_imatrix b);
 /* fmatrix */
 t_fmatrix			ft_fmatrixtheta(size_t n, size_t m);
 void				ft_fmatrixfree(t_fmatrix *matrix);
