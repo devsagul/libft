3877ce17ce4a63d3fe4c069c81576c7e66b40a91
Manfred dondarrion Balon swann <mbalon-s@e4r5p3.21-school.ru>
2019-01-30 18:02:57 +0300

    added ft_imatrixsub

---
+++ ft_imatrixsub.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_imatrixsub.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbalon-s <mbalon-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/30 18:01:53 by mbalon-s          #+#    #+#             */
+/*   Updated: 2019/01/30 18:02:27 by mbalon-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_imatrix			ft_imatrixsub(t_imatrix a, t_imatrix b)
+{
+	t_imatrix	res;
+	size_t		i;
+	size_t		j;
+	int			value1;
+	int			value2;
+
+	if (a.n != b.n || a.m != b.m)
+		return (ft_imatrixidentity(0));
+	res = ft_imatrixtheta(a.n, a.m);
+	if (res.values == NULL)
+		return (res);
+	i = 0;
+	while (i < a.n)
+	{
+		j = 0;
+		while (j < a.m)
+		{
+			ft_imatrixget(a, i, j, &value1);
+			ft_imatrixget(b, i, j, &value2);
+			ft_imatrixset(res, i, j, value1 - value2);
+			j++;
+		}
+		i++;
+	}
+	return (res);
+}
