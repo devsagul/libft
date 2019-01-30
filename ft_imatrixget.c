122ececa0cc31a494fec40452db45921a0a90b3c
Manfred dondarrion Balon swann <mbalon-s@e4r5p3.21-school.ru>
2019-01-30 17:04:05 +0300

    fixed ft_imatrixget

--- ft_imatrixget.c
+++ ft_imatrixget.c
@@ -6,7 +6,7 @@
 /*   By: mbalon-s <mbalon-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/30 16:59:33 by mbalon-s          #+#    #+#             */
-/*   Updated: 2019/01/30 17:02:42 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2019/01/30 17:03:55 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ int		ft_imatrixget(t_imatrix matrix, size_t i, size_t j, int *value)
 {
 	if (i >= matrix.n || j >= matrix.m)
 		return (0);
-	*value = matrix.values[i * matrix.m + j]
+	*value = matrix.values[i * matrix.m + j];
 	return (1);
 }
