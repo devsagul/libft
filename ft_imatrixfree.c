d7792a616a24364c782dc9940dc0a23da45299b1
Manfred dondarrion Balon swann <mbalon-s@e4r5p3.21-school.ru>
2019-01-30 18:45:37 +0300

    fixed ft_imatrixfree

--- ft_imatrixfree.c
+++ ft_imatrixfree.c
@@ -6,7 +6,7 @@
 /*   By: mbalon-s <mbalon-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/30 18:42:27 by mbalon-s          #+#    #+#             */
-/*   Updated: 2019/01/30 18:44:03 by mbalon-s         ###   ########.fr       */
+/*   Updated: 2019/01/30 18:45:26 by mbalon-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void				ft_imatrixfree(t_imatrix *matrix)
 	if (matrix->values == NULL || matrix->n == 0 || matrix->m == 0)
 		return ;
 	free(matrix->values);
+	matrix->values = NULL;
 	matrix->n = 0;
 	matrix->m = 0;
 }
